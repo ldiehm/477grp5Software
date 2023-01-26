d2fe8cef732a5b5afe018418c9bbae58e0203c4f
ldiehm <70343907+ldiehm@users.noreply.github.com>
2023-01-26 12:30:19 -0500

    Update main.c
    I (jonathon) added this under lukes account

--- main.c
+++ main.c
@@ -11,6 +11,9 @@ void nano_wait(unsigned int n) {
 
 #define ONE (38)
 #define ZERO (19)
+#define RS (0)
+#define NUMLED (10)
+int count = 0;
 
 uint16_t buf[24] = {38,38,38,38,38,38,38,38,
 				  19,19,19,19,19,19,19,19,
@@ -25,6 +28,12 @@ uint16_t green[24] = {38,38,38,38,38,38,38,38,
 uint16_t blue[24] = { 19,19,19,19,19,19,19,19,
 				  	  19,19,19,19,19,19,19,19,
 					  38,38,38,38,38,38,38,38};
+uint16_t white[24] = {38,38,38,38,38,38,38,38,
+						38,38,38,38,38,38,38,38,
+						38,38,38,38,38,38,38,38};
+uint16_t reset[24] = {RS, RS, RS, RS, RS, RS, RS, RS,
+						RS, RS, RS, RS, RS, RS, RS, RS,
+						RS, RS, RS, RS, RS, RS, RS, RS};
 
 void init_tim1_dma(void){
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
@@ -45,12 +54,8 @@ void init_tim1_dma(void){
     //TIM1->DIER |= TIM_DIER_UIE;
 
 
-    TIM1->CCMR1 &= ~TIM_CCMR1_OC1M;// | TIM_CCMR1_OC2M);
-//    TIM1->CCMR2 &= ~(TIM_CCMR2_OC3M | TIM_CCMR2_OC4M);
-
-    TIM1->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1);// | TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1);
-//    TIM1->CCMR2 |= (TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1);
-
+    TIM1->CCMR1 &= ~TIM_CCMR1_OC1M;
+    TIM1->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1);
     TIM1->CCER |= TIM_CCER_CC1E;// | TIM_CCER_CC2E | TIM_CCER_CC3E | TIM_CCER_CC4E);
 
 
@@ -59,20 +64,30 @@ void init_tim1_dma(void){
     RCC->AHBENR |= RCC_AHBENR_DMA1EN;
 
     DMA1_Channel2->CPAR = (uint32_t) (&(TIM1->CCR1)); /* (3) */
-    DMA1_Channel2->CMAR = (uint32_t)(buf); /* (4) */
+    DMA1_Channel2->CMAR = (uint32_t)(green); /* (4) */
     DMA1_Channel2->CNDTR =  (uint16_t) 24; /* (5) */
     DMA1_Channel2->CCR |= DMA_CCR_MINC | DMA_CCR_MSIZE_0 | DMA_CCR_PSIZE_0 | DMA_CCR_TEIE | DMA_CCR_TCIE ; /* (6) */
-    DMA1_Channel2->CCR |= DMA_CCR_DIR | DMA_CCR_CIRC;
+    DMA1_Channel2->CCR |= DMA_CCR_CIRC | DMA_CCR_DIR;
     DMA1_Channel2->CCR |= DMA_CCR_EN; /* (7) */
     //DMACSELR &= ~(0xf0);
     /* Configure NVIC for DMA */
     /* (1) Enable Interrupt on DMA Channel 1 */
     /* (2) Set priority for DMA Channel 1 */
-    //NVIC_EnableIRQ(DMA1_Channel2_IRQn); /* (1) */
-    //NVIC_SetPriority(DMA1_Channel2_IRQn,0); /* (2) */
+    NVIC_EnableIRQ(DMA1_Ch2_3_DMA2_Ch1_2_IRQn); /* (1) */
+    NVIC_SetPriority(DMA1_Ch2_3_DMA2_Ch1_2_IRQn,0); /* (2) */
 
     TIM1->CR1 |= TIM_CR1_CEN;
     return;
+
+}
+
+void DMA1_CH2_3_DMA2_CH1_2_IRQHandler(void){
+	count++;
+	if(count >= NUMLED){
+
+		TIM1->CCR1 = 0;
+		nano_wait(500000);
+	}
 
 }
 
@@ -80,19 +95,14 @@ void init_tim1_dma(void){
 int main(void)
 {
 
-    // Demonstrate part 1
-//#define TEST_TIMER3
-#ifdef TEST_TIMER3
-    setup_tim3();
-    for(;;) { }
-#endif
 
+	init_tim1_dma();
 
-    init_tim1_dma();
-    //init_tim7();
-    int st = 0;
+	int st = 0;
     while(1){
-    	/*
+/*
+		TIM1->CR1 &= ~TIM_CR1_CEN;
+		DMA1_Channel2->CCR &= ~DMA_CCR_EN;
     	//do color stuff here
     	if(st == 0){
         	memcpy(buf, red, 24);
@@ -101,11 +111,17 @@ int main(void)
         	memcpy(buf, green, 24);
         	st++;
         }if(st == 2){
-        	memcpy(buf, blue, 24);
+        	memcpy(buf, green, 24);
 			st++;
+        }if(st == 3){
+        	memcpy(buf, reset, 24);
+        	st = 0;
         }
-        nano_wait(50000);
-		*/
+
+        nano_wait(500000);
+		TIM1->CR1 |= TIM_CR1_CEN;
+		DMA1_Channel2->CCR |= DMA_CCR_EN;
+*/
 	}
 }
 
