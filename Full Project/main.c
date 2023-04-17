866406908ee0559f61e31bac43a90559b00cb6ae
ldiehm <70343907+ldiehm@users.noreply.github.com>
2023-04-17 17:48:26 -0400

    This code runs on the dev board but not ours

--- Full Project/main.c
+++ Full Project/main.c
@@ -3,229 +3,246 @@
 #include <string.h>
 #include "wifi.h"
 
-#define WifiCommBuffSIZE 250
-extern volatile char wifiCommunicationBuffer[WifiCommBuffSIZE];
+//constant definitions
 
-void init_usart5(void)
-{
+#define IPSIZE 16
+#define ONE (38)
+#define ZERO (19)
+#define RS (0)
+#define NUMLED (64)
+#define LED_ARR_SIZE (24*NUMLED + 200)
+#define WifiCommBuffSIZE (250)
 
-    RCC->AHBENR |= (RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIODEN);
-    GPIOC->MODER &= ~GPIO_MODER_MODER12;
-    GPIOC->MODER |= GPIO_MODER_MODER12_1; //PC12 - USART5_TX
-    GPIOD->MODER &= ~GPIO_MODER_MODER2;
-    GPIOD->MODER |= GPIO_MODER_MODER2_1; //PD2 - USART5_RX
+volatile char wifiCommunicationBuffer[WifiCommBuffSIZE];
+int count = 0;
 
-    GPIOC->AFR[1] &= ~GPIO_AFRH_AFR12;
-    GPIOC->AFR[1] |= 2 << (4 * 4);
+void nano_wait(unsigned int n) {
+	asm( "        mov r0,%0\n"
+			"repeat: sub r0,#83\n"
+			"        bgt repeat\n" : : "r"(n) : "r0", "cc");
+}
 
-    GPIOD->AFR[0] &= ~GPIO_AFRL_AFR2;
-    GPIOD->AFR[0] |= 2 << (4 * 2);
-    //done with alternate function stuff
+//BUFFER AT END FOR OFF CYCLE
+uint8_t leds[LED_ARR_SIZE] = { 0 };
 
-    RCC->APB1ENR |= RCC_APB1ENR_USART5EN;
+uint8_t off[24] = { 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
+		19, 19, 19, 19, 19, 19, 19, 19, 19 };
+uint8_t red[24] = { 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 38, 38, 38,
+		38, 19, 19, 19, 19, 19, 19, 19, 19 };
+uint8_t green[24] = { 19, 19, 19, 19, 38, 38, 38, 38, 19, 19, 19, 19, 19, 19,
+		19, 19, 19, 19, 19, 19, 19, 19, 19, 19 };
+uint8_t blue[24] = { 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
+		19, 19, 19, 19, 19, 38, 38, 38, 38 };
+uint8_t white[24] = { 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38, 38,
+		38, 38, 38, 38, 38, 38, 38, 38, 38, 38 };
+uint8_t reset[24] = { RS, RS, RS, RS, RS, RS, RS, RS,
+RS, RS, RS, RS, RS, RS, RS, RS,
+RS, RS, RS, RS, RS, RS, RS, RS };
 
-    USART5->CR1 &= ~USART_CR1_UE; // disabling usart5 for time being
-    USART5->CR1 &= ~(USART_CR1_M | 1 << 28);//(1<<12 | 1<<28); // word length of 8, making m0 and m1 0
-    USART5->CR2 &= ~USART_CR2_STOP; //making stop bit of 1
-    USART5->CR2 &= ~USART_CR1_PCE; //disabling parity
-    USART5->CR1 &= ~USART_CR1_OVER8; // oversample of 16
+void init_clock(void) {
 
-    USART5->BRR = 0x1A1; // 115.2k baud rate
-    USART5->CR1 |= (USART_CR1_TE | USART_CR1_RE);
 
-    //USART5->RTOR |= USART_RTOR_RTO;
-    USART5->CR1 |= USART_CR1_UE;
 
-    //waiting for receive enable acknowledge flag, Transmit enable acknowledge flag
-    while(!(USART5->ISR & USART_ISR_TEACK) && !(USART5->ISR & USART_ISR_REACK));
+//	CRS->CR &= ~CRS_CR_CEN;
+//	CRS->CFGR |= CRS_CFGR_SYNCSRC_0;
+//	CRS->CR |= CRS_CR_CEN;
+
+//	 (++) Modify the CPU clock source, using "RCC_SYSCLKConfig()" function
+//	         (++) If needed, modify the CPU clock prescaler by using "RCC_HCLKConfig()" function
+//	         (++) Check that the new CPU clock source is taken into account by reading
+//	              the clock source status, using "RCC_GetSYSCLKSource()" function
+
+	RCC_HCLKConfig(0); //Clear all bits of HPRE
+//	   pllmull = RCC->CFGR & RCC_CFGR_PLLMULL;
+//	      pllsource = RCC->CFGR & RCC_CFGR_PLLSRC;
+//	      pllmull = ( pllmull >> 18) + 2;
+
+	RCC->CFGR |= RCC_CFGR_PLLMUL6;
+	RCC->CFGR2 &= ~0xF; // Clears lowest 4 bits of CFGR2 (PREDIV)
+	RCC->CFGR |= RCC_CFGR_PLLSRC_0;
+	RCC->CR |= RCC_CR_PLLON;
+
+
+
+	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+	uint8_t clksrc = RCC_GetSYSCLKSource();
+	//RCC_ClocksTypeDef * clk;
+	//RCC_GetClocksFreq(clk); <- this infinite loops
+
+
+//	RCC_PLLConfig(RCC_PLLSource_HSI, RCC_CFGR_PLLMUL_2);
+
+	return;
 
 }
+
+void init_tim1_dma(void) {
+
+//    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+//    GPIOA->MODER &= ~(GPIO_MODER_MODER8);
+//    GPIOA->MODER |= GPIO_MODER_MODER8_0;
+//    GPIOA->ODR |= 1<<8;
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER &= ~(GPIO_MODER_MODER8);
+	GPIOA->MODER |= GPIO_MODER_MODER8_1;
+	GPIOA->AFR[1] &= ~(0xf << 0);
+	GPIOA->AFR[1] |= (0x2 << (0 * 4));
+
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+	TIM1->BDTR |= TIM_BDTR_MOE;
+
+	TIM1->PSC = 1 - 1;
+	TIM1->ARR = 60 - 1;
+
+	TIM1->CR2 |= TIM_CR2_CCDS; //cc DMA selection
+	TIM1->DIER |= TIM_DIER_CC1DE | TIM_DIER_UDE | TIM_DIER_TDE; // CC 1 DMA request enable
+	//TIM1->DIER |= TIM_DIER_UIE;
+
+	TIM1->CCMR1 &= ~TIM_CCMR1_OC1M;
+	//TIM1->CCMR1 |= TIM_CCMR1_OC1PE;
+	TIM1->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1PE);
+	TIM1->CCER |= TIM_CCER_CC1E;
+
+	//DMA CONF
+
+//    good stuff
+	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+
+	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
+	DMA1_Channel2->CPAR = (uint32_t) (&(TIM1->CCR1)); /* (3) */
+	DMA1_Channel2->CMAR = (uint32_t) (leds); /* (4) */
+	DMA1_Channel2->CNDTR = (uint16_t) LED_ARR_SIZE; /* (5) */
+	DMA1_Channel2->CCR |= DMA_CCR_MINC | DMA_CCR_PSIZE_0 | DMA_CCR_TEIE
+			| DMA_CCR_TCIE; /* (6) */
+	DMA1_Channel2->CCR |= DMA_CCR_DIR;    // | DMA_CCR_CIRC;
+	DMA1_Channel2->CCR |= DMA_CCR_DIR | DMA_CCR_CIRC;
+
+	TIM1->CR1 |= TIM_CR1_CEN;
+
+	return;
+
+}
+//I dont think this is even configured
+
+//void DMA1_CH2_3_DMA2_CH1_2_IRQHandler(void){
 //
-//void USART3_4_5_6_7_8_IRQHandler(void){
-//	int count = 0;
-//	while(wifiCommunicationBuffer[count] != 0){
-//		count++;
-//	}
-//	wifiCommunicationBuffer[count] = USART5->RDR;
+//	//if(DMA1->ISR & DMA_ISR_TCIF2){
+//
+//	//DMA1_Channel2->CCR &= ~DMA_CCR_EN;
+//	//TIM1->CCR1 = 0;
+//	DMA1->IFCR |= 0xf0;
+//
+//		//DMA1_Channel2->CCR |= DMA_CCR_EN;
+//		//count = 0;
+//	//}
+//
 //}
 
-void enable_DMAinterrupt(char *buf, int size) {
+void LED_write(uint8_t *leds) { //I dont think this works
+	//enable DMA to transfer array (noncircular)
+	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
+	DMA1_Channel2->CMAR = (uint32_t) leds;
+	DMA1_Channel2->CCR |= DMA_CCR_EN;
 
-    USART5->CR1 |= USART_CR1_RXNEIE | USART_CR1_RE;
-
-//	NVIC_EnableIRQ(USART3_8_IRQn);
-
-    USART5->CR3 |= USART_CR3_DMAR;
-
-    RCC->AHBENR |= RCC_AHBENR_DMA2EN;
-    DMA2->RMPCR |= DMA_RMPCR2_CH2_USART5_RX;
-
-    DMA2_Channel2->CCR &= ~DMA_CCR_EN;  // First make sure DMA is turned off
-
-    DMA2_Channel2->CPAR = &(USART5->RDR);
-    DMA2_Channel2->CMAR = &(wifiCommunicationBuffer);
-    DMA2_Channel2->CNDTR = WifiCommBuffSIZE;
-
-    DMA2_Channel2->CCR &= ~DMA_CCR_DIR;
-    DMA2_Channel2->CCR &= ~(DMA_CCR_TEIE | DMA_CCR_HTIE);
-
-    DMA2_Channel2->CCR &= ~DMA_CCR_MSIZE;
-    DMA2_Channel2->CCR &= ~DMA_CCR_PSIZE;
-
-    DMA2_Channel2->CCR |= DMA_CCR_MINC;
-    DMA2_Channel2->CCR &= ~DMA_CCR_PINC;
-
-    DMA2_Channel2->CCR |= DMA_CCR_CIRC;
-
-    DMA2_Channel2->CCR &= ~DMA_CCR_MEM2MEM;
-
-    DMA2_Channel2->CCR |= DMA_CCR_PL;
-
-    DMA2_Channel2->CCR |= DMA_CCR_EN;
-
+	return;
 }
 
+void LED_alternate(uint8_t *leds, uint8_t *col1, uint8_t *col2) {
+	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
 
-void writeString(char * input) {
-//	char *OK = "AT\r\n";
-//	for(int i = 0; i < sizeof(input); i++){
-	int i = 0;
-	while(input[i] != '\0'){
-//        while (!(USART5->ISR & USART_ISR_RXNE)) { }
-//        char c =  (USART5->RDR);
-        while(!(USART5->ISR & USART_ISR_TXE)) { }
-        nano_wait(100);
-        USART5->TDR = input[i];
-		i++;
-    }
-}
-
-void disconnectWifi(void){
-
-	writeString("AT+CWQAP\r\n");
-
-}
-
-void configureWifi(char * SSID, char * pass){
-
-
-	writeString("AT+CWJAP=\"");
-	writeString(SSID);
-	writeString("\",\"");
-	writeString(pass);
-	writeString("\"\r\n");
-
-}
-
-void configureWifiSystem(void){
-	writeString("\r\n");
-	int wait = 1000000;
-	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
-	//nano_wait(100000);
-	writeString("AT+CWMODE=1\r\n");
-
-	//nano_wait(10000000000);
-	while(!strstr(wifiCommunicationBuffer, "OK"));
-	nano_wait(100000);
-	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
-	//nano_wait(100000);
-	configureWifi("BOXFISH", "BOX");
-	//for(long long int i = 0; i < 20000000; i++);
-//	nano_wait(10000000000000);
-
-	while(!strstr(wifiCommunicationBuffer, "OK"));
-//	nano_wait(100000);
-	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
-//	nano_wait(100000);
-	writeString("AT+CIPMUX=1\r\n");
-	nano_wait(100000);
-	while(!strstr(wifiCommunicationBuffer, "OK"));
-	nano_wait(100000);
-	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
-	nano_wait(100000);
-	configureUDP("0.0.0.0");
-	nano_wait(100000);
-	while(!strstr(wifiCommunicationBuffer, "OK") && !strstr(wifiCommunicationBuffer, "ERROR"));
-	nano_wait(100000);
-	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
-}
-
-void configureUDP(char * IP){
-
-
-	writeString("AT+CIPSTART=0,\"UDP\",\"");
-	writeString(IP);
-	writeString("\",80,80,1");
-//	writeString(pass);
-	writeString("\r\n");
-
-}
-
-void clear_buf(char *buf, int size){
-
-
-	for(int i = 0; i < size; i++){
-		buf[i] = 0;
+	for (int i = 0; i < NUMLED; i++) {
+		for (int j = 0; j < 24; j++) {
+			if (i % 2)
+				leds[i * 24 + j] = col1[j];
+			else
+				leds[i * 24 + j] = col2[j];
+		}
 	}
+	DMA1_Channel2->CCR |= DMA_CCR_EN;
 
-	DMA2_Channel2->CCR &= ~DMA_CCR_EN;
-
-
-    DMA2_Channel2->CNDTR = WifiCommBuffSIZE;
-	DMA2_Channel2->CCR |= DMA_CCR_EN;
 }
 
-int match_string(char *str, char *substr, int len){
+void transferData(void) {
+	char current;
+	for (int i = 0; i < (3 * NUMLED); i++) { //add offset to buf
+		current = wifiCommunicationBuffer[i + 13];
+		for (int j = 0; j < 8; j++) {
+			if ((current & (1 << j)) != 0) {
+				leds[(i * 8) + 7 - j] = 38;
+			} else {
+				leds[(i * 8) + 7 - j] = 19;
+			}
+		}
+	}
+	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
+}
+
+//////////////////////////////////////////////////
+
+int main(void) {
+
+	char *buf;
+	buf = wifiCommunicationBuffer;
+
+	uint32_t clk = SystemCoreClock;
+	init_clock();
+	clk = SystemCoreClock;
+	init_usart5();
+
+	enable_DMAinterrupt(wifiCommunicationBuffer, WifiCommBuffSIZE);
+
+	init_tim1_dma();
+
+	LED_alternate(leds, blue, red);
+
+
+	configureWifiSystem();
+
+	LED_alternate(leds, blue, green);
+
+
 	int count = 0;
-	char *b = substr;
-	char *a = str;
-	while(count < len){
-		if (*str != *b) {
-				str++;
-				count++;
-			    continue;
-		}
-		a = str;
-		while (1) {
-			if (*b == 0) {
-				return 1;
-			}
-			if (*a++ != *b++) {
-				break;
-			}
-		}
-		b = substr;
-		str++;
-		count++;
-	}
-	return 0;
-}
+	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
 
-//Long term problem - buf potentially circling to end of buffer w/ IP
-void getIP(char * IP, char * buf, int size){
-	int i;
-	int j = 0;
-	char isIP = 0;
-	for(i = 0; i < size; i++){
+//    while(1){
+//
+//    	if(count == 0 && strstr(wifiCommunicationBuffer, "g")){
+//    	  	LED_alternate(leds, green, green);
+//    	  	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
+//
+//    	}else if(count == 1000000 && strstr(wifiCommunicationBuffer, "IPD,0,3:b")){
+//    	   	LED_alternate(leds, blue, blue);
+//    	   	clear_buf(wifiCommunicationBuffer, WifiCommBuffSIZE);
+//
+//    	} else if(count > 2000000) {
+//    		count = -1;
+//    	}
+//    	count++;
+//
+// 	}
 
-		if(buf[i] == '"'){
-			if(isIP){
-				break;
-			}
-			isIP = 1;
-		}
-		else if(isIP){
-			IP[j] = buf[i];
-			j++;
-		}
+	while(1) {
+	    if(strstr(wifiCommunicationBuffer, "IPD,0,193:")) {
+	    	while(wifiCommunicationBuffer[205] != 'K');
+	    	transferData();
+	    }
+
 	}
 
-}
-
-void establishConnection_TCP(char * IP){
-
-	writeString("AT+CIPSTART=\"TCP\",\"");
-	writeString(IP);
-	writeString("\",80\r\n");
-
+//	while (1) {
+//
+//		if (count == 0) {
+//			LED_alternate(leds, green, red);
+//
+//		} else if (count == 1000000) {
+//			LED_alternate(leds, red, green);
+//
+//		} else if (count > 2000000) {
+//			count = -1;
+//		}
+//		count++;
+//
+//	}
 }
