14be0a6ee000d8b8c0dbab48f5d1f46e09ef59d0
Jonathon Snyder <j.snyder98774@gmail.com>
2023-04-18 22:32:51 -0400

    Updated IP's to work with both displays
    This code correctly sends to both displays. If it doesn't work, try adjusting the IP addresses.

--- ESP32_toCommit/main.py
+++ ESP32_toCommit/main.py
@@ -12,44 +12,48 @@ WiFi = ESP32_WiFi_init(SSID = "BOXFISH", password = "BOX")
 
 ble = bluetooth.BLE()
 p = ESP32_BLE(ble=ble, wifi=WiFi)
-_BUFF_SIZE = 3*64
+_BUFF_SIZE = 3*64 + 1
 #ble = ESP32_BLE("LED Controller", WiFi) #, advertising_payload)
 buf1 = bytearray(_BUFF_SIZE)
 buf2 = bytearray(_BUFF_SIZE)
-count = 0
+
+
+
+buf1[_BUFF_SIZE - 1] = bytes('K', 'utf-8')[0]
+buf2[_BUFF_SIZE - 1] = bytes('K', 'utf-8')[0]
+
+count1 = 0
+count2 = 0
 def on_rx(v, wifi):
     #TODO add count that resets the byte array each frame
     global buf1
     global buf2
-    global count
-    if(count < _BUFF_SIZE):
+    global count1
+    global count2
+    if(count1 < _BUFF_SIZE - 1):
         for i in range(16):
-#                 print(type((count).to_bytes(1, "little")))
-#                 print(type(buf1[count + i]))
-            buf1[count + i] = count #(count).to_bytes(1, "little")
-#             else:
-#                 fakeVariable = 0
-#                 buf1[count + i] = 0 #(fakeVariable).to_bytes(1, "little")             
-#             buf1[count+i] = v[i]
+            #if not (count1 + i) % 3: 
+            #    buf1[count1 + i] = int((count1 + i) / 2) #(count).to_bytes(1, "little")
+            buf1[count1+i] = int(v[i] / 32)
+        count1 += 16
+    
     else:
         for i in range(16):
-            buf2[count-192+i] = v[i]
+            buf2[count2+i] = int(v[i] / 32)
         
-    count += 16
-    if(count >= 2 * _BUFF_SIZE):
-        #print("SENDING: ", len(buf1))
-#         print(len(buf2))
-#         print(buf1)
-#         bq =
-        #bq = [0] * 192
-        #for i in range(1,192,3):
-        #    bq[i] = i
-        #bq = bytearray(bq)
+        count2 += 16
+        
+    if(count2 >= _BUFF_SIZE - 1):
+        
         print(buf1)
-        wifi.s.sendto(buf1, ("192.168.4.2", 80))
-        #wifi.s.sendto(bytes((str(buf2)), "utf-8"), ("192.168.4.3", 80))
+        print(buf2)
+
+        wifi.s.sendto(buf1, ("192.168.4.3", 80))
+        wifi.s.sendto(buf2, ("192.168.4.4", 80))
         
-        count = 0
+        count1 = 0
+        count2 = 0
+
         
 p.on_write(on_rx)
 #while True:
